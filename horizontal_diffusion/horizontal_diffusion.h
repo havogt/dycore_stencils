eb6e0b1348c53eeab7e10a2a3ef971c1f7271641
Carlos Osuna <carlos.osuna@env.ethz.ch>
2016-07-04 11:12:01 +0200

    fix

--- horizontal_diffusion/horizontal_diffusion.h
+++ horizontal_diffusion/horizontal_diffusion.h
@@ -7,7 +7,4 @@
 #include "../repository.hpp"
 #include "../timer_cuda.hpp"
 
-__global__
-void cukernel(Real* in, Real* out, const int, const int, const int);
-
 void launch_kernel(repository& repo, timer_cuda*);
